0b0ba7a436287cd9fc33876c44d1919e558146dd
e-las <omonigho.research@gmail.com>
2023-09-11 03:36:20 -0700

    All task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - prints *and that piece of art is useful* - Dora korpar, 2015-10-19*,
+ * followed by a newline, to standard error.
+ * return: Always 1.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2025-10-19\n", 59);
+	return (1);
+}
