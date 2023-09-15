ee1430f6263bf2f0dba36ce5663a16b7703f5b25
e-las <omonigho.research@gmail.com>
2023-09-15 23:00:43 +0100

    Debugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - A program that Test function for positive or negative
+ * Return: each time 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
+
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This deals with Debugging which is the process of finding and fixing errors in software that prevents it from running correctly
