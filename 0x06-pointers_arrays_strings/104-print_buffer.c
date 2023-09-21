3dd531aea3f369f0af54aec8bffa78e861a35f84
e-las <omonigho.research@gmail.com>
2023-09-21 10:48:37 +0100

    C-pointers_update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,7 +10,7 @@
 
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int o, y, k;
 
 	o = 0;
 
@@ -21,22 +21,23 @@ void print_buffer(char *b, int size)
 	}
 	while (o < size)
 	{
-		j = size - o < 10 ? size - o : 10;
+		y = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
+		for (k = 0; k < 10; k++)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
+			if (k < y)
+				printf("%02x", *(b + o + k));
 				else
 					printf("  ");
-				if (i % 2)
+				if (k % 2)
 				{
 					printf(" ");
 				}
 		}
-		for (i = 0; i < j; i++)
+		for (k = 0; k < y; k++)
 		{
-			int c = *(b + o + i);
+			int c = *(b + o + k);
+
 			if (c < 32 || c > 132)
 			{
 				c = '.';
