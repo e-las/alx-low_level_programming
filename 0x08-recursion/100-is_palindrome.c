8bef7cdc356738fab49c7d1ab52d85367e86c505
e-las <omonigho.research@gmail.com>
2023-09-28 18:26:46 +0100

    advanced task solution

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int check_pal(char *s, int i, int len);
+int check_pal(char *s, int k, int len);
 int _strlen_recursion(char *s);
 
 /**
@@ -32,17 +32,17 @@ int _strlen_recursion(char *s)
 /**
  * check_pal - checks the characters recursively for palindrome
  * @s: string to check
- * @i: iterator
+ * @k: iterator
  * @len: length of the string
  *
  * Return: 1 if palindrome, 0 if not
  */
-int check_pal(char *s, int i, int len)
+int check_pal(char *s, int k, int len)
 {
-	if (*(s + i) != *(s + len - 1))
+	if (*(s + k) != *(s + len - 1))
 	return (0);
-	if (i >= len)
+	if (k >= len)
 	return (1);
-	return (check_pal(s, i + 1, len - 1));
+	return (check_pal(s, k + 1, len - 1));
 }
 
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * wildcmp - Compare strings
- * @s1: pointer to string params
- * @s2: pointer to string params
+ * wildcmp - a program that compare strings
+ * @s1: pointer to string parameter
+ * @s2: pointer to string parameter
  * Return: 0
  */
 
