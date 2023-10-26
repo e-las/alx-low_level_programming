4cd19696e70ab2016b2f0bb97b3edbca11f89d40
e-las <omonigho.research@gmail.com>
2023-10-26 16:32:09 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,18 +12,17 @@ unsigned int binary_to_uint(const char *b)
 
 	numba = 0;
 	if (!b)
-	return (0);
+		return (0);
 	for (k = 0; b[k] != '\0'; k++)
 	{
-	if (b[k] != '0' && b[k] != '1')
-	return (0);
+		if (b[k] != '0' && b[k] != '1')
+			return (0);
 	}
 	for (k = 0; b[k] != '\0'; k++)
 	{
-	numba <<= 1;
-	if (b[k] == '1')
-	numba += 1;
+		numba <<= 1;
+		if (b[k] == '1')
+			numba += 1;
 	}
 	return (numba);
 }
-
